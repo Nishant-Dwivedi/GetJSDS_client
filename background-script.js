a43a5f52d4f3da6e9c53f268bd9bb3a9f74483a3
nishant dwivedi <thenishant30@gmail.com>
2024-08-21 12:59:35 +0530

    added disclaimer about the clipboard read permission to the onboarding page

--- background-script.js
+++ background-script.js
@@ -1,10 +1,10 @@
-browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
-  switch (reason) {
-    case "install":
-      {
-        const url = browser.runtime.getURL("./onboard/installed.html");
-        await browser.tabs.create({ url });
-      }
-      break;
+browser.runtime.onInstalled.addListener(handleInstall);
+
+async function handleInstall({ reason }) {
+  if (reason == "install") {
+    const url = browser.runtime.getURL("./onboard/installed.html");
+    await browser.tabs.create({ url });
+    browser.runtime.onInstalled.removeListener(handleInstall);
   }
-});
+  return;
+}
